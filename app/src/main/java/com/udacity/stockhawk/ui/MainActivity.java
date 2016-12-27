82e339d8394479b1272bdb48b25cc39c025df482
Justin <jraczak@gmail.com>
2016-12-27 15:56:28 -0800

    Update recyler view item on click to launch stock detail view and pass in stock symbol

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.stockhawk.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -44,6 +45,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onClick(String symbol) {
         Timber.d("Symbol clicked: %s", symbol);
+        Intent intent = new Intent(this, StockDetailActivity.class);
+        intent.putExtra("symbol", symbol);
+        startActivity(intent);
+        //TODO: See what else makes sense to pass in here
     }
 
     @Override
--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
@@ -1,17 +1,22 @@
 package com.udacity.stockhawk.ui;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.udacity.stockhawk.R;
 
-public class StockDetailActivity extends Activity {
+public class StockDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_detail);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //TODO: Figure out why this is broken
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView symbolTextView = (TextView) findViewById(R.id.label_stock_symbol);
+        symbolTextView.setText(getIntent().getStringExtra("symbol"));
     }
 
 }
